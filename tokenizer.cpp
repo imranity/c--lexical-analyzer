fced713280b5415251a6f8598673c913f4b885be
Imran Hayder <hayderimran7@gmail.com>
2016-10-23 06:52:42 -0700

    update code

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -1,18 +1,25 @@
-
+/* CS-3120 Programming Languages Assignment # 3 
+Submitted By : Muhammad Malik
+NetID : wm6952
+CSU East Bay  */
 
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
 #include <stdio.h>
-#include <string.h>
+#include <string>
+#include <functional>
 #include <map>
 #include <vector>
 #include <fstream>
 #include <cstring>
 #include <cstdlib>
+#include <stdio.h>
+#include <string.h>
+
 using namespace std;
 
-map <string,map<string,int>> counter;
+map <string, int> classes = {{"keyword",0},{"identifier",0},{"digit",0},{"integer",0},{"real",0},{"character",0},{"alpha",0}};
 vector<string> ints={"0","1","2","3","4","5","6","7","8","9"}; 
 vector<string> keywords={"if","else","then","begin","end"}; 
 vector<string> identifiers={"(",")","[","]","+","=",",","-",";"}; 
@@ -22,16 +29,22 @@ vector<std::string>::iterator iter;
 bool isKeyword(string ch);
 bool isIdentifier(char ch);
 bool isInteger(char ch);
+bool isDigit(string ch);
+bool isReal(string ch);
+bool isChar(string ch);
 bool isAlpha(char ch);
-int cnt_keyword;
-int cnt_identifier;
-int cnt_integer;
-int cnt_alpha;
+int cnt_keyword=0;
+int cnt_identifier=0;
+int cnt_digit=0;
+int cnt_integer=0;
+int cnt_alpha=0;
+int cnt_char=0;
+int cnt_real=0;
 
 bool isKeyword(string ch){
   iter=find(keywords.begin(),keywords.end(),ch);
 if(iter != keywords.end()) {
-  counter["keyword"][ch]++;
+  classes["keyword"]++;
  return true;
 }
 else
@@ -42,7 +55,19 @@ else
 bool isIdentifier(string ch){
   iter=find(identifiers.begin(),identifiers.end(),ch);
 if(iter != identifiers.end()) {
-  counter["identifier"][ch]++;
+  classes["identifier"]++;
+ return true;
+}
+else
+{
+  return false;
+}
+}
+bool isDigit(string ch){
+  iter=find(ints.begin(),ints.end(),ch);
+if(iter != ints.end()) {
+  classes["digit"]++;
+  classes["integer"]++;
  return true;
 }
 else
@@ -51,20 +76,55 @@ else
 }
 }
 bool isInteger(string ch){
-  iter=find(ints.begin(),ints.end(),ch);
-if(iter != ints.end()) {
-  counter["integer"][ch]++;
- return true;
+   char * p ;
+   strtol(ch.c_str(), &p, 10) ;
+
+   if (*p == 0) {
+    classes["digit"]++;
+    return true;
+    
+   } 
+   else 
+    return false;
 }
-else
-{
-  return false;
-}
+bool isReal(string ch){
+ bool isreal=false;
+  if (ch.find(".") != std::string::npos)
+  {
+
+    for (int j=0; j < ch.length(); j++)
+    { 
+      if (ch[j]  != '.')
+      {  string search ;
+        search = ch[j];
+         iter=find(ints.begin(),ints.end(),search);
+          if(iter != ints.end()) {
+             isreal = true;}
+         else
+          {isreal=false;}
+      }
+    else
+      {;}
+
+    }
+  }  
+  else
+    {;}
+
+    if (isreal == true)
+    {
+    classes["real"]++;
+    return true;}
+    else 
+      {return false;}
+
+
 }
 bool isAlpha(string ch){
   iter=find(alpha.begin(),alpha.end(),ch);
 if(iter != alpha.end()) {
-  counter["alphabet"][ch]++;
+  classes["alphabet"]++;
+  classes["character"]++;
  return true;
 }
 else
@@ -72,7 +132,26 @@ else
   return false;
 }
 }
+bool isChar(string ch){
+bool ischar=false;
 
+for(int i=0;  i< ch.length(); i++ )
+{
+   if( isalpha(ch[i]) )
+     ischar = true;
+   else
+     ischar = false;
+}
+
+if(ch.length() > 1 && ischar == true ) {
+  classes["character"]++;
+ return true;
+}
+else
+{
+  return false;
+}
+}
 
 int main(int argc, char *argv[]) 
 {
@@ -103,40 +182,35 @@ int main(int argc, char *argv[])
     pch = strtok(line, "    ~`!@#$^&*_{}:<>|?");    // assigns tokenPtr to  first token in line
   while (pch != NULL)
   {
-//    printf ("%s\n",pch);
     if (isKeyword(pch)) {
       cnt_keyword++;
     } else if (isIdentifier(pch)) {
       cnt_identifier++;
+    } else if (isChar(pch)) {
+      cnt_char++;
+    } else if (isDigit(pch)) {
+      cnt_digit++;
     } else if (isInteger(pch)) {
       cnt_integer++;
+    } else if (isReal(pch)) {
+      cnt_real++;
     } else if (isAlpha(pch)) {
       cnt_alpha++;
     } else {
-      cout << "Error " << pch<< endl;
+      ;
     }
     pch = strtok (NULL, "    ~`!@#$^&*_{}:<>|?");
   }
 }
- for(auto& x: counter)
-{
-  if (x.first == "keyword"){
-  std::cout << "CLASS " << x.first <<  ":    TOTAL NUMBER OF "<< x.first<< ": " << cnt_keyword <<  '\n';
-  } else if (x.first == "integer"){
-    std::cout << "CLASS " << x.first <<  ":    TOTAL NUMBER OF "<< x.first<< ": " << cnt_integer <<  '\n';
-  } else if (x.first == "identifier"){
-    std::cout << "CLASS " << x.first <<  ":    TOTAL NUMBER OF "<< x.first<< ": " << cnt_identifier <<  '\n';
-  } else if (x.first == "alphabet"){
-    std::cout << "CLASS " << x.first <<  ":    TOTAL NUMBER OF "<< x.first<< ": " << cnt_alpha <<  '\n';
-  } else
-  ;
-  for(auto const& y : x.second) {
-  std::cout << " Element: " << y.first << "        Number of occurences => " << y.second << '\n';
-}
-}
- //   for (auto& x: counter) {
-  //          std::cout << x.first << " => " << x.second.second << '\n';
- //   }  
+std::cout << "CLASS " << "keyword" << ": " << classes["keyword"] <<  '\n';
+std::cout << "CLASS " << "identifier" << ": " << classes["identifier"] <<  '\n';
+std::cout << "CLASS " << "digit" << ": " << classes["digit"] <<  '\n';
+std::cout << "CLASS " << "integer" << ": " << classes["integer"] <<  '\n';
+std::cout << "CLASS " << "character" << ": " << classes["character"] <<  '\n';
+std::cout << "CLASS " << "alphabet" << ": " << classes["alphabet"] <<  '\n';
+std::cout << "CLASS " << "real" << ": " << classes["real"] <<  '\n';
+
+
 inFile.close();
   return 0;
 }
